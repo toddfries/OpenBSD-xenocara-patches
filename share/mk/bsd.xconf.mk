31d4dc64671369340d275355228a51f34727b23c
Todd T. Fries <todd@fries.net>
2011-03-08 15:05:38 -0600

    sync

--- share/mk/bsd.xconf.mk
+++ share/mk/bsd.xconf.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: bsd.xconf.mk,v 1.16 2010/11/27 14:20:47 matthieu Exp $
+# $OpenBSD: bsd.xconf.mk,v 1.18 2011/03/08 20:48:59 matthieu Exp $
 
 # Shared libs?
 .if ${MACHINE_ARCH} == "m88k" || ${MACHINE} == "vax"
@@ -11,12 +11,12 @@ XENOCARA_HAVE_SHARED_LIBS?=yes
 XENOCARA_BUILD_GL?=${XENOCARA_HAVE_SHARED_LIBS:L}
 XENOCARA_BUILD_DRI?=${XENOCARA_HAVE_SHARED_LIBS:L}
 
-# Build XCB?
-XENOCARA_BUILD_XCB?=yes
-
 # Build pixman?
 .if ${MACHINE} == "vax"
 XENOCARA_BUILD_PIXMAN?=no
 .else
 XENOCARA_BUILD_PIXMAN?=yes
 .endif
+
+# Build xkeyboard-config?
+XENOCARA_USE_XKEYBOARD_CONFIG?=no
